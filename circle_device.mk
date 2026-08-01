ad18d2e989228b74f1d2b858f763b9aa8b2c2c53
Prabhuswar Saha <prabhuswarppsaha@gmail.com>
2026-08-01 14:39:25 +0530

    Update and rename lineage_stone.mk to circle_device.mk

--- circle_device.mk
+++ circle_device.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device.
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common CiRCLE stuff.
+$(call inherit-product, vendor/circle/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
@@ -24,7 +24,7 @@ PRODUCT_BRAND := Redmi
 PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 12 5G
-PRODUCT_NAME := lineage_stone
+PRODUCT_NAME := circle_stone
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
