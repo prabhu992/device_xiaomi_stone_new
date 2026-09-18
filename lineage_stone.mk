2b1355bdefb68f57dbb6db0cbd9a988ed3b2f3a7
Prabhuswar Saha <prabhuswarppsaha@gmail.com>
2026-09-18 20:07:53 +0530

    Update lineage_stone.mk

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -32,3 +32,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="sunstone_global-user 14 UKQ1.240624.001 OS2.0.5.0.UMQMIXM release-keys" \
     BuildFingerprint=Redmi/sunstone_global/sunstone:14/UKQ1.240624.001/OS2.0.5.0.UMQMIXM:user/release-keys \
     DeviceProduct=sunstone
+
+# Build flags
+WITH_GMS := false
+TARGET_INCLUDE_VIPERFX := true
+TARGET_INCLUDE_GRAMOPHONE := true
+TARGET_ENABLE_BLUR := false
